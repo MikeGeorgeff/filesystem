72f1ba758eeac52bf9e1bc345064eaad5483f972
Mike Georgeff <mikegeorgeff3@gmail.com>
2015-11-25 11:14:53 -0600

    add a factory class

---
+++ spec/FactorySpec.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 11/25/15
+ * Time: 11:10 AM
+ */
---
+++ src/Factory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 11/25/15
+ * Time: 11:07 AM
+ */
+
+namespace Georgeff\Filesystem;
+
+
+class Factory
+{
+
+}
